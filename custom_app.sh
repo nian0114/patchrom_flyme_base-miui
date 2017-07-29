ea707ceb680fd68dc73c901c9e6e28f1a752daa3
nian0114 <268078545@qq.com>
2017-07-29 10:41:12 +0800

    fix phone fc

---
+++ custom_app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apkBaseName=$1
+tempSmaliDir=$2
+GIT_APPLY=$PORT_ROOT/tools/git/git.apply
+
+function applyPatch() {
+    cd $1
+    for patch in `find . -name "*.patch"`
+    do
+	cp $patch ../$2/
+        cd ../$2
+        $GIT_APPLY $patch
+	rm -rf *.patch
+        cd -
+    done
+    cd ..
+}
+
+
+if [ $1 = "TeleService" ];then
+	applyPatch $1 $2
+fi
+
+
